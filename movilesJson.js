62fc65a39dbe0eea4247e368a91f1b00d8b2e350
xXpisesXx <pisesrock0.1@gmail.com>
2026-02-09 15:07:54 -0400

    Create movilesJson.js

---
+++ movilesJson.js
@@ -0,0 +1,12 @@
+const clientes = [
+    { id: 1, movil: "00", nombre: "Wilson Mamani Ojeda", tel: "59170000001", deudas: [{ concepto: "mes septiembre", monto: 35.00 }, { concepto: "mes octubre", monto: 35.00 }, { concepto: "mes noviembre", monto: 35.00 }, { concepto: "mes diciembre", monto: 35.00 }, { concepto: "mes enero", monto: 35.00 }] },
+    { id: 2, movil: "01", nombre: "Pablo Siles", tel: "59174839540", deudas: [{ concepto: "Suscripción Anual", monto: 1200.00 }] },
+    { id: 3, movil: "02", nombre: "Héctor Vargas", tel: "59171405978", deudas: [{ concepto: "mes octubre", monto: 55.00 }, { concepto: "mes noviembre", monto: 150.00 }, { concepto: "mes diciembre", monto: 155.00 }, { concepto: "mes enero", monto: 155.00 }] },
+    { id: 4, movil: "03", nombre: "Alberto Medina", tel: "59170000004", deudas: [{ concepto: "Servicio Técnico", monto: 300.00 }] },
+    { id: 5, movil: "04", nombre: "Juan Mamani", tel: "59170000005", deudas: [{ concepto: "Consultoría", monto: 500.00 }, { concepto: "Extras", monto: 50.00 }] },
+    { id: 6, movil: "06", nombre: "Irineo Paniagua", tel: "59170000006", deudas: [] },
+    { id: 7, movil: "07", nombre: "Wilson Mamani Mamani", tel: "59170000007", deudas: [] },
+    { id: 8, movil: "09", nombre: "Rodrigo Fuentes", tel: "59170000008", deudas: [] },
+    { id: 9, movil: "11", nombre: "Elvis Calle", tel: "59170000009", deudas: [] },
+    { id: 10, movil: "12", nombre: "Manuel", tel: "59170000010", deudas: [] }
+];
